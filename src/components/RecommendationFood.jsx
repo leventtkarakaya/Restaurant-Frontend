aa981173990c001a83ff03f6dad111e9e7be65d1
Levent KARAKAYA <139760691+leventtkarakaya@users.noreply.github.com>
2024-06-25 19:47:23 +0300

    Update RecommendationFood.jsx

--- src/components/RecommendationFood.jsx
+++ src/components/RecommendationFood.jsx
@@ -14,7 +14,7 @@ export default function RecommendationFood() {
     setUploading(true);
     try {
       const res = await axios.get(
-        "https://restaurant-backend-seven.vercel.app/api/v1/food/admin/getTopRating"
+        "https://restaurant-backend-drab.vercel.app/api/v1/food/admin/getTopRating"
       );
       setUploading(false);
       if (res.data.success && uploading === false) {
